f4b0e1c6e4f66efbdb13ef7ebbe7e56b0c20c50f
Automated <actions@users.noreply.github.com>
2023-11-15 20:34:00 -0800

    Fixed date format.

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ set -o pipefail
 set -eu
 
 mkdir -p data
-curl https://www.nytimes.com/svc/connections/v1/$(date +%Y-%M-%d).json >data/$(date +%Y-%M-%d).json
+curl https://www.nytimes.com/svc/connections/v1/$(date +%Y-%m-%d).json >data/$(date +%Y-%m-%d).json
 git config user.name "Automated"
 git config user.email "actions@users.noreply.github.com"
 git add data/*.json
